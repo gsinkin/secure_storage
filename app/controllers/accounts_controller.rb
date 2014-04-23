1d7116561b9491ae5b2bd4a85997d4a969c67e36
Gabe Sinkin <gsinkin@ebay.com>
2014-04-23 10:46:24 -0700

    Adding more CRUD crap

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,17 +1,42 @@
 class AccountsController < ApplicationController
-  def new
+  def index
+    @accounts = Account.all
+  end
 
+  def new
+    @account = Account.new
   end
 
   def create
     @account = Account.new(account_params)
 
-    @account.save
-    redirect_to @account
+    if @account.save
+      redirect_to @account
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  def edit
+    @account = Account.find(params[:id])
+  end
+
+  def update
+    @account = Account.find(params[:id])
+
+    if @account.update(account_params)
+      redirect_to @account
+    else
+      render 'edit'
+    end
   end
 
   private
     def account_params
-      params.require(:accounts).permit(:email)
+      params.require(:account).permit(:email)
     end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,3 @@
 class Account < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true, email_format: { message: "not a valid email address" }
 end
